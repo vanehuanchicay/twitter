558cc11c8e1edee9d3e6a81c4f1223a894cea6f1
vanehuanchicay <vanesapardoh@gmail.com>
2017-11-07 20:46:30 -0300

    actualización

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,46 @@
+function contar() {
+  var max = "140";
+  var text = document.getElementById("ingreso").value;
+  var long = text.length;
+
+    if(long <= max){
+      document.getElementById("number").value = max-long;
+     } else {
+      document.getElementById("number").value = text.substr(0, max);
+  }
+}
+
 
 function add(){
 
-    //llamo a mi ID ingreso donde iran los pensamientos
+    //llamo a mi ID ingreso donde irán los pensamientos
     var comment = document.getElementById("ingreso").value;
 
     //Limpio mi cuadro de ingreso
-    document.getElementById("ingreso").value == "";
+    document.getElementById("ingreso").value = "";
 
 
     // si ingresa algo vacio retorne un alerta
-    if (document.getElementById("ingreso").value === ""){
+    if (comment === ""){
       alert("Debes ingresar un pensamiento");
-
     }
 
     else {
-      var cont = document.getElementById("contador");
+      var cont = document.createElement("div"); // creando contenedor
+      cont.className = "contenedor"  // dando una clase al contenedor
       var nameSpan = document.createElement("span"); // span con el nombre de usuario
+      nameSpan.className ="user"
       var dateSpan = document.createElement("span"); // span con fecha de ingreso
+      dateSpan.className ="date"
       var tweetP = document.createElement("p"); // parrafo de pensamientos
 
-      // agrego mis elementos creados al contenedor contador
+      // agrego mis elementos creados al contenedor
       cont.appendChild(nameSpan);
       cont.appendChild(dateSpan);
       cont.appendChild(tweetP);
 
-      nameSpan.textContent = "Michael Myers ";
-      dateSpan.textContent = new Date();
+      nameSpan.textContent = "Michael Myers";
+      dateSpan.textContent = new Date() ;
       tweetP.textContent = comment;
 
 
